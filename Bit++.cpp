eebf48469dc7fa68f9631eebcbb31e2f1d91d10f
Anish_Jabras <anishjabras2@gmail.com>
2026-08-24 19:08:14 +0530

    Bit++ Problem

---
+++ Bit++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int x = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        string statement;
+        cin >> statement;
+
+        if (statement[1] == '+')
+            x++;
+        else
+            x--;
+    }
+
+    cout << x;
+
+    return 0;
+}
